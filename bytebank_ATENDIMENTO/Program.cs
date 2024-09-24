01c807dbc8a68c6e0fe4bceae42e98ef050ee6b8
Tácio Moreira <145516170+TacioMoreira25@users.noreply.github.com>
2024-09-23 21:26:17 -0300

    Add files via upload

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,16 @@
+﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+//using bytebank_GeradorChavePix;
+
+new ByteBankAtendimento().AtendimentoCliente();
+
+// *Demostração para usar o gerador de chaves aleatórias*
+//Console.WriteLine(GeradorPix.GetChavePix());
+
+// var listaDeChaves = GeradorPix.GetChavesPix(2);
+
+// foreach (var chave in listaDeChaves)
+// {
+//     Console.WriteLine(chave);
+// }
+
+
